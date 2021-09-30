fbec509b0a53f397949f809c2639d544c350478e
Carter Vande Moore <72159057+cvmoore@users.noreply.github.com>
2021-09-30 09:57:43 -0400

    sim.py created

--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,3 +1,4 @@
+import numpy as np
 def euclidean_dist(x, y):
     res = 0
     for i in range(len(x)):
@@ -5,12 +6,22 @@ def euclidean_dist(x, y):
     return res**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    res = 0
+    for i in range(len(x)):
+        res += (x[i] - y[i])
+    return res
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    res = (len(x.intersection(y)) / len(x.union(y)))
+    return res
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    disY = 0
+    disX = 0
+    for i in range(len(x)):
+        disX = disX + x[i]
+        disY = disY + y[i]
+    num = np.dot(np.transpose(x) * y) / (disX * disY)
+    return num
 
 # Feel free to add more
